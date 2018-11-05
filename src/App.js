387677fb421976c3d2cd8f161669a643cd883395
completejavascript <completejavascript.super@gmail.com>
2018-11-05 21:34:01 +0700

    update image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
 
-import DraggableNode from './components/draggable-note/draggable-note';
+import ModalImage from './components/modal-image/modal-image';
+import img6 from './images/07.jpg';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h2>Sticky Notes</h2>
-        {/* <div>
-          Made by <a href="https://about.phamvanlam.com/">Lam Pham</a>.
-          Visit me at <a href="https://completejavascript.com/">completejavascript.com</a>.
-        </div> */}
+        <h2>Image Modal</h2>
+        <p>Click the image below to show the modal.</p>
 
-        <DraggableNode
-          title={`Click here to move the note`}
-          width={`400px`}
-          height={`250px`}
-          top={`50px`}
-          left={`200px`}
-          content={``}
+        <ModalImage 
+          src={img6}  
+          alt={`This is one of beautiful girls`}
+          ratio={`3:2`}
         />
+
+        <div>
+          Made by <a href="https://about.phamvanlam.com/">Lam Pham</a>. 
+          Visit me at <a href="https://completejavascript.com/">completejavascript.com</a>.
+        </div>
       </div>
     );
   }
