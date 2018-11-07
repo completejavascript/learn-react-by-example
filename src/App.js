4d82d02b070db0b0ae9c80a51ce2b61f63d63cba
completejavascript <completejavascript.super@gmail.com>
2018-11-07 23:00:49 +0700

    add PUBLIC_URL to deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import createHistory from 'history/createBrowserHistory';
 
 import SlideshowApp from './App.slideshow';
 import SlideshowGalleryApp from './App.slideshow-gallery';
 import ModalImageApp from './App.modal-image';
 import LightBoxApp from './App.lightbox';
 
+const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
+
 export default class App extends React.Component {
   render() {
     return (
-      <Router className="App">
+      <Router className="App" history={history} basename={process.env.PUBLIC_URL}>
         <div>
           <Route exact path="/slideshow/" component={SlideshowApp} />
           <Route exact path="/slideshow-gallery/" component={SlideshowGalleryApp} />
