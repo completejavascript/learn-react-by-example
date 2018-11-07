2811f45d7b914a91f3505a11913272c1ac4ff35c
completejavascript <completejavascript.super@gmail.com>
2018-11-07 22:33:31 +0700

    add PUBLIC_URL to deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import createHashHistory from 'history/createHashHistory';
 
 import SlideshowApp from './App.slideshow';
 import SlideshowGalleryApp from './App.slideshow-gallery';
@@ -10,12 +11,12 @@ import LightBoxApp from './App.lightbox';
 export default class App extends React.Component {
   render() {
     return (
-      <Router className="App">
+      <Router className="App" basename={process.env.PUBLIC_URL}>
         <div>
-          <Route exact path={`${process.env.PUBLIC_URL}/slideshow/`} component={SlideshowApp} />
-          <Route exact path={`${process.env.PUBLIC_URL}/slideshow-gallery/`} component={SlideshowGalleryApp} />
-          <Route exact path={`${process.env.PUBLIC_URL}/modal-image/`} component={ModalImageApp} />
-          <Route exact path={`${process.env.PUBLIC_URL}/lightbox/`} component={LightBoxApp} />
+          <Route exact path="/slideshow/" component={SlideshowApp} />
+          <Route exact path="/slideshow-gallery/" component={SlideshowGalleryApp} />
+          <Route exact path="/modal-image/" component={ModalImageApp} />
+          <Route exact path="/lightbox/" component={LightBoxApp} />
         </div>
       </Router>
     );
