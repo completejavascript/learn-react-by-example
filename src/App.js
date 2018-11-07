4f43fa53f73d47934928a492d88c38766328b110
completejavascript <completejavascript.super@gmail.com>
2018-11-08 00:22:45 +0700

    add hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import SlideshowApp from './App.slideshow';
 import SlideshowGalleryApp from './App.slideshow-gallery';
@@ -9,17 +9,15 @@ import LightBoxApp from './App.lightbox';
 
 export default class App extends React.Component {
   render() {
-    console.log("basename", process.env.PUBLIC_URL);
-    
     return (
-      <BrowserRouter className="App" basename={process.env.PUBLIC_URL}>
-        <div>
+      <HashRouter className="App">
+        <Switch>
           <Route exact path="/slideshow/" component={SlideshowApp} />
           <Route exact path="/slideshow-gallery/" component={SlideshowGalleryApp} />
           <Route exact path="/modal-image/" component={ModalImageApp} />
           <Route exact path="/lightbox/" component={LightBoxApp} />
-        </div>
-      </BrowserRouter>
+        </Switch>
+      </HashRouter>
     );
   }
 }
